6b066f33756dca397ed856207936781873aeeeea
perch9 <kortek90@mail.ru>
2022-12-28 19:55:36 +0300

    init

---
+++ shop.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    document.getElementById("btn").addEventListener('click',function(event){
+        document.getElementById("myDropdown").classList.toggle("show");
+    })
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+})
